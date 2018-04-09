208f3a69c5713c685a6b7fd9020b56f9a3e8ad7f
Lam Nguyen <lnguyen@netsuite.com>
2018-04-08 19:01:47 -0500

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -14,15 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Gas bill', amount: 200, createdAt: 1500}));
-store.dispatch(addExpense({description: 'Rent bill', amount: 0, createdAt: 500}));
-
-var state = store.getState();
-var expenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(expenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
