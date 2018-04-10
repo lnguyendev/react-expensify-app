60fba51eee507e6c7aaef30f7fddffeccff22059
Lam Nguyen <lnguyen@netsuite.com>
2018-04-10 01:48:57 -0500

    Setup production build and server

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage' ;
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
@@ -16,7 +15,6 @@ const AppRouter = () => (
                 <Route path="/" component={ExpenseDashBoardPage} exact={true} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
