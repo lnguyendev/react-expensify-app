dcfc11a9876a94da9c5f28a6d2adcca128804ebd
Lam Nguyen <lnguyen@netsuite.com>
2018-04-22 20:43:27 -0500

    Add Expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({expenses, total}) => (
-    <div>
-        <p>
-            Viewing {expenses.length} expense{expenses.length > 1 ? 's' : ''} totalling {numeral(total).format('$0,0.00')}
-        </p>
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header-title">
+                Viewing <span>{expenses.length}</span> expense{expenses.length > 1 ? 's' : ''} totalling <span>{numeral(total).format('$0,0.00')}</span>
+            </h1>
+            <div className="page-header-actions">
+                <Link className="button" to="/create">Add Expense</Link>
+            </div>
+        </div>
     </div>
 );
 
